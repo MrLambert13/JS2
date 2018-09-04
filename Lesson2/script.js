26ae7fe2eec9a90f1d675d3c2b1bb55c8c133f36
Mr.L@mbert_13 <Lam_13@mail.ru>
2018-09-04 08:49:39 +0300

    crash all works

--- Lesson2/script.js
+++ Lesson2/script.js
@@ -5,11 +5,24 @@ window.onload = function () {
     xhr.open('GET', 'http://localhost:3000/items');
     xhr.send();
     xhr.onreadystatechange = function () {
-      if (xhr.readyState === XMLHttpRequest.DONE){
+      if (xhr.readyState === XMLHttpRequest.DONE) {
         var items = JSON.parse(xhr.responseText);
+
         items = items.map(function (item) {
-          return new MenuItem(item.href, item.label);
+          if (item.sub) {
+            console.log(item.sub[0]);
+            return new MenuItem(item.href, item.label, new SubMenu('submenu', 'submenu', new MenuItem(
+              function (item) {
+                return [item.sub[0].href, item.sub[0].label];
+              }
+              )
+              )
+            );
+          } else {
+            return new MenuItem(item.href, item.label);
+          }
         });
+
         var menu = new Menu('menu', 'menu', items);
 
         document.body.appendChild(menu.render());
