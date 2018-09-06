887c574ddc3c265077782f2157792682e9e7a20f
Mr.L@mbert_13 <lam_13@mail.ru>
2018-09-06 11:07:12 +0300

    HW-3 Ex.2 complete

--- Lesson3/script.js
+++ Lesson3/script.js
@@ -1,7 +1,12 @@
 window.onload = function () {
 
+  /**
+   * Функция заменяет все однарные кавычки на двойные, кроме конструкции "n't" d аглоязычной верси.
+   * @param text {string} Передаваемый иходный текст
+   * @return {string} обработанный текст
+   */
   function changeText(text) {
-    return text.replace(/\'/g, `"`);
+    return text.replace(/(?!=n)\'(?!t)/g, `"`);
   }
 
   document.getElementById('btn').addEventListener('click', function (event) {
