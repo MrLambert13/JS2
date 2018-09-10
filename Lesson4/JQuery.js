f9ec210e903de8aebc1237a89aab5403f692bf7d
Mr.L@mbert_13 <Lam_13@mail.ru>
2018-09-10 21:48:45 +0300

    lesson 4 ex 1 complete

--- Lesson4/JQuery.js
+++ Lesson4/JQuery.js
@@ -1,3 +1,29 @@
 "use strict";
 
-@import()
+function getJsonText(idTarget, callback) {
+  var result;
+  $.ajax({
+    url: 'http://localhost:3000/texts',
+    dataType: "json",
+    success: function (data) {
+      $.each(data, function (i, val) {
+          if (val.id === idTarget) {
+            result = val.text;
+          }
+        }
+      );
+      callback(result);
+    }
+  });
+}
+
+(function ($) {
+  $(function () {
+    $('#tabs').on('click', '.tab', function (event) {
+      getJsonText(+event.target.dataset.id, function (result) {
+        $('.text')[0].innerText = result.toString();
+      });
+    });
+  });
+})(jQuery);
+
