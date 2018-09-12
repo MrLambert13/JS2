bf0e48163861a2ae6e9f5c791b4ec0c7d5414dae
Mr.L@mbert_13 <lam_13@mail.ru>
2018-09-12 16:38:42 +0300

    форма и валидация

--- test/form.js
+++ test/form.js
@@ -3,5 +3,22 @@
 var validation = {
   phone: /^\+7\(\d{3}\)\d{3}\-\d{4}$/,
   email: /^[-._a-z0-9]+@([a-z0-9][-a-z0-9]+\.)+[a-z]{2.6}$/i,
+  name: /^\w{3,}$/
+};
 
+window.onload = function () {
+  document.getElementById('submit').addEventListener('click', function (event) {
+    Object.keys(validation).forEach(function (rule) {
+      var fields = document.querySelectorAll('[data-validation-rule="' + rule + '"]');
+      console.log(fields);
+      fields.forEach(function (field) {
+        if (validation[rule].test(field.value)) {
+          field.classList.remove('invalid');
+        } else {
+          field.classList.add('invalid');
+        }
+      });
+    });
+    event.preventDefault();
+  });
 };
