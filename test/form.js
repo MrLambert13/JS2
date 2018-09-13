c8b96550ef0f5f321750a4a47125fd23fa3faee8
Mr.L@mbert_13 <lam_13@mail.ru>
2018-09-13 08:39:47 +0300

    форма и валидация

--- test/form.js
+++ test/form.js
@@ -6,11 +6,26 @@ var validation = {
   name: /^\w{3,}$/
 };
 
+(function ($) {
+  $(function () {
+    $('#submit').click(function (event) {
+      $(Object.keys(validation)).each(function (rule) {
+        var fields = $('[data-validation-rule="' + rule + '"]');
+        $(fields).each(function (field) {
+          debugger;
+          (validation[rule].test(field.value) ? $(field).addClass('invalid') : $(field).removeClass('invalid'));
+        });
+      });
+      event.preventDefault();
+    });
+  });
+})(jQuery);
+
+/*
 window.onload = function () {
   document.getElementById('submit').addEventListener('click', function (event) {
     Object.keys(validation).forEach(function (rule) {
       var fields = document.querySelectorAll('[data-validation-rule="' + rule + '"]');
-      console.log(fields);
       fields.forEach(function (field) {
         if (validation[rule].test(field.value)) {
           field.classList.remove('invalid');
@@ -21,4 +36,4 @@ window.onload = function () {
     });
     event.preventDefault();
   });
-};
+};*/
