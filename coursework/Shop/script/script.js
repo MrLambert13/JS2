b4069b518deb6cb924229e9d3c1debd69f2a9303
Mr.L@mbert_13 <Lam_13@mail.ru>
2018-09-21 13:27:25 +0300

    удаляю товары из product.html

--- coursework/Shop/script/script.js
+++ coursework/Shop/script/script.js
@@ -1,1 +1,13 @@
 "use strict";
+
+$('document').ready(function () {
+  loadGoods();
+});
+
+function loadGoods() {
+  //load goods from goods.json
+  $.getJSON('json/goods.json', function (data) {
+    console.log(data);
+
+  });
+}
