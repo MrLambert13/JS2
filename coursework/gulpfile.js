5f78e9c72f1cea828c66c19824f5f65ca46e93fc
Mr.L@mbert_13 <Lam_13@mail.ru>
2018-09-22 12:28:44 +0300

    create watch with *.sass *.js *.html

--- coursework/gulpfile.js
+++ coursework/gulpfile.js
@@ -2,3 +2,27 @@ var gulp = require('gulp'),
   sass = require('gulp-sass'),
   browserSync = require('browser-sync');
 
+//прогоняем sass файлы
+gulp.task('sass', function () {
+  return gulp.src('src/sass/**/*.sass')
+    .pipe(sass())
+    .pipe(gulp.dest('src/style'))
+    .pipe(browserSync.reload({string: true}));
+});
+
+//настройки сервера для browser sync
+gulp.task('browserSync', function () {
+  browserSync({
+    server: {
+      baseDir: 'src'
+    }
+  })
+});
+
+//watcher
+gulp.task('watch', function () {
+  gulp.watch('src/sass/**/*sass', ['sass']);
+  gulp.watch('src/*.html', browserSync.reload);
+  gulp.watch('src/script/**/*.js', browserSync.reload);
+});
+
